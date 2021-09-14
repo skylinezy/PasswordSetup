710456da33de0e070d72fe63c8baa23db6f44056
skylinezy <developer.yuanzi@gmail.com>
2021-09-14 10:56:04 -0400

    Step2

--- PasswordSetup/PasswordSetupView.swift
+++ PasswordSetup/PasswordSetupView.swift
@@ -7,9 +7,37 @@
 
 import SwiftUI
 
+struct PasswordPolicy: OptionSet {
+    let rawValue: Int8
+    
+    static let hasUppercase = PasswordPolicy(rawValue: 1<<0)
+    static let hasLowercase = PasswordPolicy(rawValue: 1<<1)
+    static let hasNumber    = PasswordPolicy(rawValue: 1<<2)
+    static let hasSymbol    = PasswordPolicy(rawValue: 1<<3)
+}
+
 struct PasswordSetupView: View {
     @State var password: String = ""
     @State var confirmPassword: String = ""
+    
+    var policy: PasswordPolicy = [.hasUppercase, .hasLowercase, .hasNumber, .hasSymbol]
+    @State var policyMeet: PasswordPolicy = []
+    var minLength: Int = 6
+    
+    let lc = "abcdefghijklmnopqrstuvwxyz"
+    let uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let nb = "0123456789"
+    let sb = "~`!@#$%^&*()-_=+{}[]:;<>,.?/"
+    
+    func checkPassword() -> Void {
+        policyMeet = []
+        for l in password {
+            if lc.contains(l) {policyMeet.insert(.hasLowercase)}
+            if uc.contains(l) {policyMeet.insert(.hasUppercase)}
+            if nb.contains(l) {policyMeet.insert(.hasNumber)}
+            if sb.contains(l) {policyMeet.insert(.hasSymbol)}
+        }
+    }
     
     func savePassword() -> Void {
         print("Saved password: \(password)")
@@ -23,11 +51,91 @@ struct PasswordSetupView: View {
                 .padding(5)
                 .disableAutocorrection(true)
                 .autocapitalization(.none)
+                .onChange(of: password) { newPassword in
+                    checkPassword()
+                }
+            
+            VStack(alignment: .leading, spacing: 2) {
+                HStack() {
+                    Image(systemName: "checkmark.circle")
+                        .font(.footnote)
+                        .foregroundColor(.green)
+                        .opacity(password.count >= minLength ? 1.0 : 0.0)
+                    Text("Must be \(minLength) characters or longer")
+                        .font(.footnote)
+                        .foregroundColor(password.count >= minLength ? .green : .red)
+                        .opacity(1.0)
+                }
+                
+                if policy.contains(.hasUppercase) {
+                    HStack() {
+                        Image(systemName: "checkmark.circle")
+                            .font(.footnote)
+                            .foregroundColor(.green)
+                            .opacity(policyMeet.contains(.hasUppercase) ? 1.0 : 0.0)
+                        Text("Must contain at least 1 upper case")
+                            .font(.footnote)
+                            .foregroundColor(policyMeet.contains(.hasUppercase) ? .green : .red)
+                    }
+                }
+                
+                if policy.contains(.hasLowercase) {
+                    HStack() {
+                        Image(systemName: "checkmark.circle")
+                            .font(.footnote)
+                            .foregroundColor(.green)
+                            .opacity(policyMeet.contains(.hasLowercase) ? 1.0 : 0.0)
+                        Text("Must contain at least 1 lower case")
+                            .font(.footnote)
+                            .foregroundColor(policyMeet.contains(.hasLowercase) ? .green : .red)
+                        
+                    }
+                }
+                
+                if policy.contains(.hasNumber) {
+                    HStack() {
+                        Image(systemName: "checkmark.circle")
+                            .font(.footnote)
+                            .foregroundColor(.green)
+                            .opacity(policyMeet.contains(.hasNumber) ? 1.0 : 0.0)
+                        Text("Must contain at least 1 number")
+                            .font(.footnote)
+                            .foregroundColor(policyMeet.contains(.hasNumber) ? .green : .red)
+                        
+                    }
+                }
+                
+                if policy.contains(.hasSymbol) {
+                    HStack() {
+                        Image(systemName: "checkmark.circle")
+                            .font(.footnote)
+                            .foregroundColor(.green)
+                            .opacity(policyMeet.contains(.hasSymbol) ? 1.0 : 0.0)
+                        Text("Must contain at least 1 symbol")
+                            .font(.footnote)
+                            .foregroundColor(policyMeet.contains(.hasSymbol) ? .green : .red)
+                        
+                    }
+                }
+            }
+            
             TextField("Re-enter Password", text: $confirmPassword)
                 .textFieldStyle(PlainTextFieldStyle())
                 .padding(5)
                 .disableAutocorrection(true)
                 .autocapitalization(.none)
+            
+            HStack() {
+                Image(systemName: "checkmark.circle")
+                    .font(.footnote)
+                    .foregroundColor(.green)
+                    .opacity((password.count > 0 && password == confirmPassword) ? 1.0 : 0.0)
+                Text("Password match")
+                    .font(.footnote)
+                    .foregroundColor((password.count > 0 && password == confirmPassword) ? .green : .red)
+                    .opacity((password.count > 0 && password == confirmPassword) ? 1.0 : 0.0)
+            }
+            
             Button(action:{
                 if password.count>0 && password == confirmPassword {
                     savePassword()
@@ -51,6 +159,10 @@ struct PasswordSetupView: View {
 
 struct PasswordSetupView_Previews: PreviewProvider {
     static var previews: some View {
-        PasswordSetupView()
+        Group {
+            PasswordSetupView()
+                .previewDevice("iPhone 12 mini")
+            PasswordSetupView()
+        }
     }
 }
