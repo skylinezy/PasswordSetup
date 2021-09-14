771bbe077aae41ff82d14ebbdbed01db3044e1a9
skylinezy <developer.yuanzi@gmail.com>
2021-09-13 20:13:43 -0400

    Initial Commit

---
+++ PasswordSetup/PasswordSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordSetupApp.swift
+//  PasswordSetup
+//
+//  Created by Ryan Zi on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct PasswordSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
